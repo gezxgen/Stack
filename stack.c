1e7ed68c650bd261fd4b9b843ec798e3c3900df8
kevin <kevin@ammersinn.ch>
2024-08-28 15:10:09 +0200

    add files

--- stack.c
+++ stack.c
@@ -1,5 +1,48 @@
-//
-// Created by kevin on 28.08.2024.
-//
+/* Imports */
+#import <stdlib.h>
+#import <stdio.h>
+#include <conio.h>
+#import "stack.h"
 
-#include "stack.h"
+char get_main(void) {
+        int func_eingabe = 0;
+        printf("---------- Funktionsmenu ----------\n");
+        printf("0 - Load stack\n");
+        printf("1 - Append\n");
+        printf("2 - Pop last\n");
+        printf("3 - Pop first\n");
+        printf("4 - Stack count\n");
+        printf("5 - Stack reset\n");
+        printf("6 - Stack info\n");
+        printf("7 - Terminate\n");
+        func_eingabe = getch();
+
+        return func_eingabe;
+}
+
+void print_stack(int *first, int length) {
+        printf("---------- Stackinfo ----------\n");
+        for (int i=0; i<length; i++) {
+                printf("%d\t- %d\n", i, *first);
+                first += 1;
+        }
+        printf("\n");
+}
+
+int * append(int *first, int length, int num) {
+        if (first == NULL) {
+                int *new_stack = malloc(1 * sizeof(int));
+                new_stack[0] = num;
+                return new_stack;
+        } else {
+                int *new_stack = malloc((length + 1) * sizeof(int));
+
+                for (int i = 0; i < length; i++) {
+                        new_stack[i] = first[i];
+                }
+
+                new_stack[length] = num;
+                free(first);
+                return new_stack;
+        }
+}
--- stack.h
+++ stack.h
@@ -1,8 +1,8 @@
-
-
 #ifndef STACKS_STACK_H
 #define STACKS_STACK_H
 
-
+char get_main(void);
+void print_stack(int *, int);
+int * append(int *, int, int);
 
 #endif //STACKS_STACK_H
