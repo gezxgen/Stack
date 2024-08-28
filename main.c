37e396e81537716ea9bd40982d433ca5ed346a96
kevin <kevin@ammersinn.ch>
2024-08-28 11:00:24 +0200

    add main and gitignore

--- main.c
+++ main.c
@@ -1,14 +1,80 @@
+/* Imports */
 #import <stdlib.h>
 #import <stdio.h>
+#include <conio.h>
 
+/* Defines */
+#define LENGTH 20
 
+/* Defines */
+int get_main(void);
+
+/* Main */
 int main() {
-        /* Vairables */
+        /* Variables */
         char end;
+        int eingabe = 0;
+        int stack[LENGTH];
 
         /* Test */
-        printf("Hello World\n");
+        while (eingabe != 7) {
+                eingabe = get_main();
+
+                switch (eingabe) {
+                /* Load stack */
+                case 1:
+                        printf("%d", eingabe);
+                        break;
+
+                /* Load stack */
+                case 2:
+                        printf("%d", eingabe);
+                        break;
+
+                /* Load stack */
+                case 3:
+                        printf("%d", eingabe);
+                        break;
+
+                /* Load stack */
+                case 4:
+                        printf("%d", eingabe);
+                        break;
+
+                /* Load stack */
+                case 5:
+                        printf("%d", eingabe);
+                        break;
+
+                /* Load stack */
+                case 6:
+                        printf("%d", eingabe);
+                        break;
+
+                /* Wrong entry */
+                default:
+                        printf("Invalid entry");
+                }
+        }
+
 
         /* Ending */
-        scanf("Druecke eine beliebige Taste... ", &end);
+        printf("Druecke eine beliebige Taste...");
+        end = getch();
+}
+
+int get_main(void) {
+        int func_eingabe = 0;
+        printf("---------- Funktionsmenu ----------\n");
+        printf("0 - Load stack\n");
+        printf("1 - Append\n");
+        printf("2 - Pop last\n");
+        printf("3 - Pop first\n");
+        printf("4 - Stack count\n");
+        printf("5 - Stack reset\n");
+        printf("6 - Stack info\n");
+        printf("7 - Terminate\n");
+        scanf("%d", &func_eingabe);
+
+        return func_eingabe;
 }
