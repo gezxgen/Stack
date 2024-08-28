570f3a9c040281e91dae0b48de43b2ed17a57617
kevin <kevin@ammersinn.ch>
2024-08-28 13:12:30 +0200

    add structure v2

--- main.c
+++ main.c
@@ -5,25 +5,23 @@
 #import "stack.h"
 
 /* Defines */
-#define LENGTH 20
-
-/* Defines */
 char get_main(void);
+void print_stack(int *, int);
+int * append(int *, int, int);
 
 /* Main */
 int main() {
         /* Variables */
         int num;
+        int length = 0;
         char eingabe = 0;
-        int stack[LENGTH];
-
-        /* Add elements */
-        for (int i=0; i<LENGTH; i++){
-                stack[i] = 0;
-        }
+        int stack[1];
+        int *first = &stack[0];
+        stack[0] = 0;
 
         /* Test */
         while (eingabe != '7') {
+                length = sizeof(stack) / sizeof(stack[0]);
                 eingabe = get_main();
 
                 switch (eingabe) {
@@ -36,10 +34,15 @@ int main() {
                 case '1':
                         printf("Which number would you like to append? ");
                         scanf("%d", &num);
-                        for (int i = 0; i < LENGTH; i++) {
-                                if (stack[i] == 0) {
-                                        stack[i] = num;
-                                        break;
+                        if (stack[0] == 0) {
+                                stack[0] = num;
+                        } else {
+                                printf("%p\n", &first);
+                                first = append(first, length, num);
+                                printf("%p\n", &first);
+                                for (int i=0; i<length+1; i++) {
+                                        stack[i] = *first;
+                                        first += 1;
                                 }
                         }
                         printf("Number added\n\n");
@@ -67,11 +70,7 @@ int main() {
 
                         /* Stack info */
                 case '6':
-                        printf("-------------------\n");
-                        for (int i = 0; i < LENGTH; i++) {
-                                printf("%d\t- %d\n", i, stack[i]);
-                        }
-                        printf("-------------------\n");
+                        print_stack(first, length);
                         break;
 
                         /* Wrong entry */
@@ -95,4 +94,26 @@ char get_main(void) {
         func_eingabe = getch();
 
         return func_eingabe;
+}
+
+void print_stack(int *first, int length) {
+        printf("---------- Stackinfo ----------\n");
+        for (int i=0; i<length; i++) {
+                printf("%d\t- %d\n", i, *first);
+                first += 1;
+        }
+        printf("\n");
+}
+
+int * append(int *first, int length, int num) {
+        int new_stack[length + 1];
+        int * new_first = &new_stack[0];
+        new_stack[length] = num;
+
+        for (int i=0; i<length; i++) {
+                new_stack[i] = *first;
+                first += 1;
+        }
+
+        return new_first;
 }
